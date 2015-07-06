b8ca77ed3f8519ddbb59281eeff97fb58682f26c
Richard Artoul <richardartoul@gmail.com>
2015-07-06 15:39:02 -0700

    [Refactor] Modified bubbleUp to send the correct payload with child_modified

--- server/utils/bubbleUp.js
+++ server/utils/bubbleUp.js
@@ -1,12 +1,13 @@
 var getQuery = require('../rethinkQuery/getQuery');
 var getParent = require('./getParent');
 var isolateData = require('./isolateData');
+var getLastKey = require('./getLastKey');
 //emits to all parents of current path AND current path. 
 var bubbleUp = function(event, path, io, inputData) {
   var rootObject;
   var data;
 
-  var recurse = function(event, path) {
+  var recurse = function(event, path, recursiveCall, childKey) {
     var parentPath;
     //if the event is 'value', will query the db for the new data for every parent path.
     if(event === 'value') {
@@ -26,7 +27,7 @@ var bubbleUp = function(event, path, io, inputData) {
       io.to(path + '-child_added').emit(path + '-child_added', inputData);
       parentPath = getParent(path);
       if(parentPath) {
-        recurse('child_changed', parentPath);
+        recurse('child_changed', parentPath, true, getLastKey(path));
         // io.to(parentPath + '-value').emit(parentPath + -'value', isolateData(parentPath, rootObject));
       }
     }
@@ -34,10 +35,18 @@ var bubbleUp = function(event, path, io, inputData) {
     //if the event is 'child_changed'
     if(event === 'child_changed') {
       // socket.emit(path + '-child_changed', inputData);
-      io.to(path + '-child_changed').emit(path + '-child_changed', inputData);
+      var payload = {};
+      //the recurse parameter is used to determine if the function was called directly or from within itself
+      if (recursiveCall) {
+        payload[childKey] = isolateData(path, rootObject)[childKey];
+      }
+      else {
+        payload = inputData;
+      }
+      io.to(path + '-child_changed').emit(path + '-child_changed', payload);
       parentPath = getParent(path);
       if(parentPath) {
-        recurse('child_changed', parentPath);
+        recurse('child_changed', parentPath, true, getLastKey(path));
         // recurse('value', parentPath);
       }
     }
@@ -48,7 +57,7 @@ var bubbleUp = function(event, path, io, inputData) {
       io.to(path + '-child_removed').emit(path + '-child_removed', inputData);
       parentPath = getParent(path);
       if(parentPath) {
-        recurse('child_changed', parentPath);
+        recurse('child_changed', parentPath, true, getLastKey(path));
         // recurse('value', parentPath);
       }
     }
@@ -63,7 +72,7 @@ var bubbleUp = function(event, path, io, inputData) {
     var parentPath = getParent(path);
     //if event is value or child_added, emit event at current path, otherwise start at parent
     if ((event === 'child_removed' || event === 'child_changed') && parentPath) {
-      recurse(event, parentPath);
+      recurse(event, parentPath, false);
     }
     //if even is child-related, emit event at parent path
     else if (event === 'child_added' && path) {
