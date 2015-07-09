3e617c16a6bc2245953bbbce5232d92d4d538853
Richard Artoul <richardartoul@gmail.com>
2015-07-09 16:13:46 -0700

    [Documentation] Added documentation for the GET API route

--- server/api/apiRouteHandlers.js
+++ server/api/apiRouteHandlers.js
@@ -1,6 +1,14 @@
 var getQuery = require('../rethinkQuery/getQuery');
 var apiToPath = require('./apiToPath');
 
+/**
+*@apiGroup REST API
+*@apiName GET
+*@api {GET} /api/* Request JSON data for a specified path. Anything after '/api/' will be interpreted as the path. For example, '/api/users/' will return
+*all the users in the database. Requesting '/api/' by itself will return root.
+*
+*@apiSuccess {JSON} pathData JSON data that represents the data at the requested path.
+*/
 var getRoute = function(req, res) {
   getQuery(apiToPath(req.url), function(getQueryResult) {
     res.status(200).json(getQueryResult);
