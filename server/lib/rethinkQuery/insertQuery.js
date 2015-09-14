cbedfe66b6aa4afb1fd2ca21bd34fb87d04d25dd
Richard Artoul <richardartoul@gmail.com>
2015-09-14 16:19:43 -0700

    updated some functions to use coroutines

--- server/lib/rethinkQuery/insertQuery.js
+++ server/lib/rethinkQuery/insertQuery.js
@@ -4,52 +4,52 @@ var parseToObj = require('../utils/parseToObj');
 var config = require('../config');
 var updateByFilter = require('./updateByFilterQuery');
 var handleError = require('./handleError');
+var Promise = require('bluebird');
 
-var insertQuery = function(input, callback) {
+var insertQuery = Promise.coroutine(function* (input, callback) {
+  var result;
   //handles empty object case
   if (Object.keys(input).length === 0) {
-    r.db(config.dbName).table(config.tableName).insert(input).run()
-    .then(function(result) {
-      if(callback) {
-        callback(result);
-      }
-    }).error(handleError);
+    result = yield r.db(config.dbName).table(config.tableName).insert(input).run();
+    if(callback) {
+      callback(result);
+    }
   }
   else {
     //convert to array if not array
     if (!(input instanceof Array)) {
       input = [input];
     }
-      var counter = 0;
-      var limit = input.length;
-      input.forEach(function(row) {
-        var rowFilter;
-        if (row.path) {
-          rowFilter = {path: row.path, _id: row._id};
+    var counter = 0;
+    var limit = input.length;
+    for (var i = 0; i < input.length; i++) {
+      var row = input[i];
+      var rowFilter;
+      if (row.path) {
+        rowFilter = {path: row.path, _id: row._id};
+      }
+      else {
+        rowFilter = {_id: row._id};
+      }
+
+      result = yield r.db(config.dbName).table(config.tableName).filter(rowFilter).run();
+      if (result.length === 0) {
+        result = yield r.db(config.dbName).table(config.tableName).insert(row).run();
+        counter++;
+        if(callback && counter === limit) {
+          callback(result);
         }
-        else {
-          rowFilter = {_id: row._id};
-        }
-        r.db(config.dbName).table(config.tableName).filter(rowFilter).run().then(function(result) {
-          if (result.length === 0) {
-            r.db(config.dbName).table(config.tableName).insert(row).run().then(function(result) {
-              counter++;
-              if(callback && counter === limit) {
-                callback(result);
-              }
-            }).error(handleError);
-          }
-          else {
-            updateByFilter(rowFilter, row, function() {
-              counter++;
-              if(callback && counter === limit) {
-                callback(result);
-              }
-            });
+      }
+      else {
+        updateByFilter(rowFilter, row, function() {
+          counter++;
+          if(callback && counter === limit) {
+            callback(result);
           }
         });
-      });
-  }
-};
+      }
+    }
+  };
+});
 
-module.exports = insertQuery;
+module.exports = insertQuery; 
--- server/lib/specs/utils/resetDb.js
+++ server/lib/specs/utils/resetDb.js
@@ -1,16 +1,16 @@
 var r = require('../../db');
 var utils = require('../configTest').utils;
 var handleError = require('../../rethinkQuery/handleError');
+var Promise = require('bluebird');
 
-var resetDb = function(callback) {
-  r.db(utils.dbName).table(utils.tableName).delete().run()
-  .then(function(result) {
-    return r.db(utils.dbName).table(utils.tableName).insert({path: null, _id: '/'}).run()
-  }).then(function(result) {
-    return r.db(utils.dbName).table(utils.tableName).insert({path: '/', _id: 'users'}).run()
-  }).then(function(result) {
-    callback();
-  }).error(handleError);
+var resetDb = Promise.coroutine(function* (callback) {
+  var result;
+  result1 = yield r.db(utils.dbName).table(utils.tableName).delete().run();
+  result2 = yield r.db(utils.dbName).table(utils.tableName).insert({path: null, _id: '/'}).run();
+  result3 = yield r.db(utils.dbName).table(utils.tableName).insert({path: '/', _id: 'users'}).run();
+  callback();
+});
+
+module.exports = function(callback) {
+  resetDb(callback).catch(function(error) {console.log(error)});
 }
-
-module.exports = resetDb;
